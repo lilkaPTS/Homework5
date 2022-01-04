0b4bf455d7a4cc4437485d40cdd7d6d5999d68bb
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2022-01-04 21:30:59 +0300

    adding swagger-ui

--- src/main/java/com/company/SpringBootStarter.java
+++ src/main/java/com/company/SpringBootStarter.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Contacts API", version = "1.0", description = "Contacts web service"))
 public class SpringBootStarter {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class, args);
